18069fa8ffef90219d0c1f8462beaec50e891d41
nhicu <55456980+nhicu@users.noreply.github.com>
2019-10-23 13:28:46 -0400

    fixed project structure

---
+++ src/hello-users.js
@@ -0,0 +1,4 @@
+const readlineSync = require("readline-sync");
+
+const name = readlineSync.question("\nHi! What's your name? \n");
+console.log("\nHello, " + name + "!\n");
---
+++ src/perimeter.js
@@ -0,0 +1,10 @@
+const readlineSync = require("readline-sync");
+const Width = readlineSync.question("\nWidth: ");
+const Length = readlineSync.question("Length: ");
+const Conversion = 2.54;
+let w = Width;
+let l = Length;
+let c = Conversion;
+let y = (2*w*c)+(2*l*c);
+let perimeter = y.toLocaleString("en", { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+console.log("\nA(n) " + w + "-by-" + l +"-inch sheet of paper has a perimeter of " + perimeter + " centimeter(s).\n");
