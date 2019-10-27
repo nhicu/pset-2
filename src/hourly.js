d0780278dd87f0a5aa70793005fdbe8a648d3530
Nhi Cu <Nhi@HUNGs-MBP.fios-router.home>
2019-10-27 16:18:29 -0400

    lol

--- src/hourly.js
+++ src/hourly.js
@@ -8,6 +8,6 @@ const Friday = Number(readlineSync.question("Friday: "));
 const Saturday = Number(readlineSync.question("Saturday: "));
 const Sunday = Number(readlineSync.question("Sunday: "));
 
-let w = (Wage*Monday) + (Wage*Tuesday) + (Wage*Wednesday) + (Wage*Thursday) + (Wage*Friday) + (Wage*Saturday) + (Wage*Sunday);
+let w = (Monday + Tuesday + Wednesday + Thursday + Friday + Saturday + Sunday)*Wage;
 let total = w.toLocaleString("en", { minimumFractionDigits: 2, maximumFractionDigits: 2 });
 console.log("\nYou'll make $" + total + " this week.\n");
