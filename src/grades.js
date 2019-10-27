8f6daf114aeb80592284854be333d6ec886133ab
Nhi Cu <Nhi@HUNGs-MBP.fios-router.home>
2019-10-27 13:41:48 -0400

    wow

--- src/grades.js
+++ src/grades.js
@@ -4,17 +4,17 @@ const Homework = 0.15
 const Quizzes = 0.35
 const Tests = 0.50
 
-const Threehw1 = readlineSync.question("\nEnter three homework grades.\n");
-const Threehw2 = readlineSync.question("");
-const Threehw3 = readlineSync.question("");
+const Threehw1 = Number(readlineSync.question("\nEnter three homework grades.\n"));
+const Threehw2 = Number(readlineSync.question());
+const Threehw3 = Number(readlineSync.question());
 
-const Threeqz1 = readlineSync.question("\nEnter three quiz grades.\n");
-const Threeqz2 = readlineSync.question("");
-const Threeqz3 = readlineSync.question("");
+const Threeqz1 = Number(readlineSync.question("\nEnter three quiz grades.\n"));
+const Threeqz2 = Number(readlineSync.question());
+const Threeqz3 = Number(readlineSync.question());
 
-const Threets1 = readlineSync.question("\nEnter three test grades.\n");
-const Threets2 = readlineSync.question("");
-const Threets3 = readlineSync.question("");
+const Threets1 = Number(readlineSync.question("\nEnter three test grades.\n"));
+const Threets2 = Number(readlineSync.question());
+const Threets3 = Number(readlineSync.question());
 
 
 let hwaverage = (Threehw1+Threehw2+Threehw3)/3;
@@ -25,5 +25,6 @@ let hwscaled = hwaverage*Homework;
 let qzscaled = qzaverage*Quizzes;
 let tsscaled = tsaverage*Tests;
 
-let grade = hwscaled + qzscaled + tsscaled;
-console.log("\nYour marking period grade is " + grade +"%.\n");
+let g = hwscaled + qzscaled + tsscaled;
+let total = g.toLocaleString("en", { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+console.log("\nYour marking period grade is " + total +"%.\n");
