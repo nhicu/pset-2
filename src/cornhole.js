79b7d98707394d9eedc1a12f142363657665aaf5
Nhi Cu <Nhi@HUNGs-MBP.fios-router.home>
2019-10-27 20:07:35 -0400

    well

---
+++ src/cornhole.js
@@ -0,0 +1,9 @@
+const readlineSync = require("readline-sync");
+let l = 48
+let w = 24
+let d = 6
+let area = l*w
+let circlearea = .25*3.14*6*6
+let total = area - circlearea
+let newtotal = total.toLocaleString("en", { minimumFractionDigits: 2, maximumFractionDigits: 2 });
+console.log("\nThe surface area of a standard Cornhole board is " + newtotal + " square inch(es).\n");
